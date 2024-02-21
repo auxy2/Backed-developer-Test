3ab2548a88b7b9dc3ed8136323a2eccf0570544c
ABDULLAHI <Ashleyalvins010@gmail.com>
2024-02-21 07:32:42 +0100

    Updated

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	// "context"
+	"errors"
+
 	"github.com/gin-gonic/gin"
 	// "net/http"
 )
@@ -20,12 +22,49 @@ var products = []product{
 	{Id: "4", Product: "microwave", Description: "digital microwave with parfect heat condition", Price: 40589},
 }
 
-func getProduts(context *gin.Context) {
+func getAllProduts(context *gin.Context) {
 	context.IndentedJSON(200, products)
+}
+
+func addProducts(context *gin.Context) {
+	var newProducts product
+
+	if err := context.BindJSON(&newProducts); err != nil {
+		return
+	}
+
+	products = append(products, newProducts)
+	context.IndentedJSON(201, newProducts)
+}
+
+func getProduct(context *gin.Context) {
+	id := context.Param("id")
+
+	product, err := getProdutsById(id)
+
+	if err != nil {
+		context.IndentedJSON(404, gin.H{"message": "Product Not Found"})
+		return
+	}
+
+	context.IndentedJSON(200, product)
+}
+
+func getProdutsById(id string) (*product, error) {
+	for i, p := range products {
+		if p.Id == id {
+			return &products[i], nil
+		}
+	}
+
+	return nil, errors.New("product not found")
 }
 
 func main() {
 	router := gin.Default()
-	router.GET("/products", getProduts)
+	router.GET("/products", getAllProduts)
+	router.POST("/addProducts", addProducts)
+	router.GET("/products/:id", getProduct)
+
 	router.Run("localhost:9090")
 }
