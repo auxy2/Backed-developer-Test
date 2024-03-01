fb88c3ede70586abb77af607dc7353d1e7ee18dd
ABDULLAHI <Ashleyalvins010@gmail.com>
2024-03-01 20:43:06 +0100

    Updated

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
+	controller "./controllers"
 	"github.com/gin-gonic/gin"
-
 )
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/products", controllers.GetAllProducts)
-	router.GET("/products/:merchantID", GetProductsByMerchant)
-	router.POST("/products", CreateProduct)
-	router.PUT("/products/:id", UpdatedProduct)
-	router.DELETE("/products/:id", DeleteProduct)
+	router.GET("/products", controller.GetAllProducts)
+	router.GET("/products/:merchantID", controller.GetProductsByMerchant)
+	router.POST("/products", controller.CreateProduct)
+	router.PUT("/products/:id", controller.UpdatedProduct)
+	router.DELETE("/products/:id", controller.DeleteProduct)
 
 	router.Run(":9090")
 }
